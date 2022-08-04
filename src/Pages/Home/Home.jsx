32d4c1ee3a01618713fabacdd948f85e3cf7cfee
NursiNursi <muhamadnursiramdan@gmail.com>
2022-08-04 08:59:09 +0700

    nambahin section1 dan 2 di homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,120 @@
 import React, { Fragment } from 'react';
-
 import Header from "../../Layout/Header/Header";
+import { useState, useEffect } from 'react'
+import './home.css'
+import section1 from '../../Assets/img/section1.png'
+import section2 from '../../Assets/img/section2.webp'
+import { WaterIcon, FireIcon, GrassIcon, GroundIcon, RockIcon, IceIcon, ElectricIcon, DragonIcon, GhostIcon, PsychicIcon, NormalIcon, FightingIcon, PoisonIcon, BugIcon, FlyingIcon, DarkIcon, FairyIcon} from '../../Assets';
 
 function Home() {
+
+  const [types, setTypes] = useState([]);
+
+  useEffect(() => {
+    setTypes([
+      {
+        icon: WaterIcon,
+        label: 'Water'
+      },
+      {
+        icon: FireIcon
+        ,
+        label: 'Fire'
+      },
+      {
+        icon: GrassIcon,
+        label: 'Grass'
+      },
+      {
+        icon: GroundIcon,
+        label: 'Ground'
+      },
+      {
+        icon: RockIcon,
+        label: 'Rock'
+      },
+      {
+        icon: IceIcon,
+        label: 'Ice'
+      },
+      {
+        icon: ElectricIcon,
+        label: 'Electric'
+      },
+      {
+        icon: DragonIcon,
+        label: 'Dragon'
+      },
+      {
+        icon: GhostIcon,
+        label: 'Ghost'
+      },
+      {
+        icon: PsychicIcon,
+        label: 'Psychic'
+      },
+      {
+        icon: NormalIcon,
+        label: 'Normal'
+      },
+      {
+        icon: FightingIcon,
+        label: 'Fighting'
+      },      
+      {
+        icon: PoisonIcon,
+        label: 'Poison'
+      },
+      {
+        icon: BugIcon,
+        label: 'Bug'
+      },
+      {
+        icon: FlyingIcon,
+        label: 'Flying'
+      },
+      {
+        icon: DarkIcon,
+        label: 'Dark'
+      },
+      {
+        icon: FairyIcon,
+        label: 'Fairy'
+      }
+    ])
+  }, [])
+
   return (
     <Fragment>
       <Header />
       <main className="container">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum laboriosam inventore voluptate odio beatae explicabo laborum eum vitae adipisci fugit, quidem aperiam possimus, reiciendis quis officia ipsa! Accusantium, tenetur atque! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet a beatae ex itaque non mollitia eos iure dignissimos, voluptatum consequuntur tempora, voluptatem et praesentium explicabo, accusantium totam est. Dignissimos ea quaerat ratione vero culpa nam doloremque, recusandae beatae rerum, non blanditiis molestiae, ab quas corrupti quasi reiciendis temporibus odio sint perferendis. Iste, repellendus minus? Porro dolores, quam, ad nam natus consequatur quasi harum reiciendis ipsam aspernatur eos repellendus, velit magnam pariatur! Modi corrupti reiciendis minima, facilis ipsa animi vero debitis possimus quo rem nemo, fuga, corporis earum ratione doloribus nobis. Laboriosam, ipsa illum. Doloremque, vel veritatis corporis ullam itaque accusantium totam ex aspernatur quisquam odit omnis quas debitis distinctio hic libero id perspiciatis eius consequuntur labore esse modi. Atque eligendi voluptas dolore fugit minus, sed ab, ea, perferendis consequatur maxime vitae labore vel impedit similique accusamus mollitia ducimus autem repudiandae incidunt asperiores omnis odio veritatis voluptates. Voluptas dignissimos quod aliquid debitis illo, facere doloribus laborum molestiae consequuntur atque. Unde inventore error sunt saepe dignissimos incidunt voluptatum atque! Dolor adipisci autem tenetur consectetur ipsa similique voluptas vero repellat quis, animi molestias, necessitatibus repudiandae minus voluptatibus sunt commodi, ullam reprehenderit est suscipit. Enim vitae, neque modi repellendus blanditiis et quibusdam eos excepturi?</p>
+      <div className="section1">
+        <img src={section1} alt="" />
+        <div>
+          <h2>Apa itu Pokémon?</h2>
+          <p>Pokémon adalah makhluk misterius dengan banyak rahasia. Beberapa Pokémon tinggal bersama manusia dan beberapa tinggal di alam bebas di padang rumput, gua, atau laut, tapi banyak hal tentang ekologi mereka yang masih belum diketahui. Salah satu fitur utama mereka adalah bahwa mereka dapat ditangkap menggunakan Poké Ball, yang memungkinkan mereka dibawa-bawa.</p>
+        </div>
+      </div>
+      <div className="section2">
+          <div>
+            <h2>Berapa banyak tipe Pokémon?</h2>
+            <p>Pokémon dikelompokkan menjadi 18 tipe berdasarkan karakteristiknya. Sebagai contoh, Pikachu adalah tipe Pokémon Elektronik dan Magikarp adalah tipe Air. Ada beberapa Pokémon yang termasuk dalam dua tipe, seperti Jigglypuff, yang merupakan tipe Normal dan Peri. Setiap tipe memiliki afinitas khusus yang memiliki pengaruh besar dalam pertarungan. Jika Pokémon milik lawanmu memiliki jenis afinitas yang tepat, itu bisa menjadi keuntungan besar dalam pertarungan.</p>
+          </div>
+          <img src={section2} alt="" />
+      </div>
+      <div className="section2-type">
+          <div className="types">
+          {types?.map((type, idx)=>(
+                <div key={idx}>
+                    <div className='icon-types'>
+                        <img src={type?.icon} alt="icon"/>
+                        <h2>{type.label}</h2>
+                    </div>
+                </div>
+              )
+            )}
+          </div>
+      </div>
       </main>
     </Fragment>
   )

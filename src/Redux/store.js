f0e8dc81cb5e87ebce5be85d33e9090e117d7d1d
HarisHerdiansyah <haris54237@gmail.com>
2022-08-05 16:25:45 +0700

    konfigurasi redux persist

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,22 @@
 // TODO write and export store here
 
 import { createStore, applyMiddleware } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import createSagaMiddleware from "redux-saga";
 import pokemonReducer from "./slice";
 import requestAPIFromSaga from "./sagas";
 
+const persistConfig = {
+  key: "pokemonReducer",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, pokemonReducer);
 const sagaMiddleware = createSagaMiddleware();
-const rootStore = createStore(pokemonReducer, applyMiddleware(sagaMiddleware));
+const rootStore = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+const persistor = persistStore(rootStore)
 
 sagaMiddleware.run(requestAPIFromSaga);
 
-export default rootStore;
+export { rootStore, persistor };
